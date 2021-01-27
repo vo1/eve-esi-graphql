eb65a9b1698bb91b66c13709837236364ed93393
Vo1 <insyon@gmail.com>
2021-01-27 10:21:31 +0100

    fix eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,20 +3,6 @@ const env = require('dotenv').config().parsed
 
 module.exports = {
   apps: [
-	// Character ESI
-    {
-      name: 'build-service-character',
-      script: 'npm build',
-      cwd: './service-character',
-      args: '',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: env.NODE_ENV,
-      },
-    },
     {
       name: 'service-character',
       script: 'npm start',
@@ -31,20 +17,6 @@ module.exports = {
         PORT: 4001,
       },
     },
-	// Corporation ESI
-	{
-      name: 'build-service-corporation',
-      script: 'npm build',
-      cwd: './service-corporation',
-      args: '',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: env.NODE_ENV,
-      },
-    },
     {
       name: 'service-corporation',
       script: 'npm start',
@@ -57,21 +29,6 @@ module.exports = {
       env: {
         ...env,
         PORT: 4002,
-      },
-    },
-
-    // Gateway
-	{
-      name: 'build-gateway',
-      script: 'npm build',
-      cwd: './gw',
-      args: '',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: env.NODE_ENV,
       },
     },
     {
